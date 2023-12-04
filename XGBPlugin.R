d59f87d427407a295092a7f450a75eb5bfeb6ac0
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2023-12-04 17:14:08 -0500

    Initial

---
+++ XGBPlugin.R
@@ -0,0 +1,29 @@
+library(AppliedPredictiveModeling)
+transparentTheme(trans = .4)
+library(caret)
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+
+
+input <- function(inputfile) {
+	pfix = prefix()
+  parameters <<- read.table(inputfile, as.is=T);
+  rownames(parameters) <<- parameters[,1];
+   # Need to get the three files
+   csvfile <<- paste(pfix, parameters["csvfile", 2], sep="/")
+
+   training <<- read.csv(csvfile)
+   fitControl <<- readRDS(paste(pfix, parameters["fitControl", 2], sep="/"))
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+set.seed(825)
+result <- train(Class ~ ., data = training, 
+                 method = "xgbDART", 
+                 trControl = fitControl)
+print(result)
+saveRDS(result, outputfile)
+}
